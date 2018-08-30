ed8d2fa16e6b9ddc7b2f51a801e280bbb9508150
Anurag Sharma <anurags2111@gmail.com>
2018-08-30 17:58:13 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,32 @@
+class Node
+{
+    String Date;
+    String data, nodeId, referenceNodeId, childReferenceNodeId, genesisReferenceNodeId, HashValue;
+    int nodeNumber;
+    Node left, right;
+
+    public Node(String item)
+    {
+        data = item;
+        left = right = null;
+    }
+
+}
+
+public class Posist {
+
+    Node root;
+
+    
+
+    int Encrypt() {
+        return 0;
+    }
+
+    int Decrypt() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+    }
+}
